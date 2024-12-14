3196d511338cb47c9da988a385ef684329f6216f
daeho.kim <kfdream123@gmail.com>
2024-12-14 19:03:34 +0900

    LeetCode 217. Contains Duplicate

---
+++ KimDaeho/Python/LeetCode_217_Contains_Duplicate.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/contains-duplicate/
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dict = {}
+        for num in nums:
+            dict[num] = dict.get(num, 0) + 1
+            if dict[num] > 1:
+                return True
+        return False
+
